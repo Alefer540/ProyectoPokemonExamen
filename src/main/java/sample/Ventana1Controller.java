fb587f0d344553ff93aa582c1b2ecf78735d3ce1
Alejandro <alefer540@alu.centroafuera.es>
2022-01-12 11:27:30 +0100

    cambios clase 21

--- src/main/java/sample/Ventana1Controller.java
+++ src/main/java/sample/Ventana1Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
@@ -14,12 +15,24 @@ import javafx.stage.StageStyle;
 
 public class Ventana1Controller {
 	int cont=0;
-	Pokemon p1 = new Pokemon("Jolteon",204f,204f,"LV.65",new Image("https://assets.pokemon.com/assets/cms2/img/pokedex/full/135.png"));
-	Pokemon p2 =new Pokemon ("Charizard",148f,148f,"LV.45",new Image("https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png"));
-	Pokemon p3=new Pokemon("Vapereon",234f,234f,"LV.54",new Image("https://assets.pokemon.com/assets/cms2/img/pokedex/full/134.png"));
-	Pokemon p4=new Pokemon("Blastoise",222f,222f,"LV.65",new Image("https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png"));
-	Pokemon p5=new Pokemon("Mewtwo",298f,298f,"LV.75",new Image("https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png"));
-	Pokemon p6=new Pokemon("Butterfree",160f,80f,"LV.54", new Image("https://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png"));
+	Pokemon p1 = new Pokemon("Jolteon",204f,204f,"LV.65",new Image("https://assets.pokemon.com/assets/cms2/img/pokedex/full/135.png"),new Image("https://cdn-icons-png.flaticon.com/512/90/90704.png"));
+	Pokemon p2 =new Pokemon ("Charizard",148f,148f,"LV.45",new Image("https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png"),new Image("https://cdn-icons-png.flaticon.com/512/32/32353.png"));
+	Pokemon p3=new Pokemon("Vapereon",234f,234f,"LV.54",new Image("https://assets.pokemon.com/assets/cms2/img/pokedex/full/134.png"),new Image("https://cdn-icons-png.flaticon.com/512/90/90704.png"));
+	Pokemon p5=new Pokemon("Blastoise",222f,222f,"LV.65",new Image("https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png"),new Image( "https://cdn-icons-png.flaticon.com/512/32/32353.png"));
+	Pokemon p4=new Pokemon("Mewtwo",298f,298f,"LV.75",new Image("https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png"),new Image("https://cdn-icons-png.flaticon.com/512/32/32353.png"));
+	Pokemon p6=new Pokemon("Butterfree",160f,160f,"LV.54", new Image("https://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png"),new Image("https://cdn-icons-png.flaticon.com/512/90/90704.png"));
+	@FXML
+	ImageView Sexo1;
+	@FXML
+	ImageView Sexo2;
+	@FXML
+	ImageView Sexo3;
+	@FXML
+	ImageView Sexo4;
+	@FXML
+	ImageView Sexo5;
+	@FXML
+	ImageView Sexo6;
 	@FXML
 	Label l1Nombre1;
 	@FXML
@@ -92,11 +105,19 @@ public class Ventana1Controller {
 	AnchorPane Anchor5;
 	@FXML
 	AnchorPane Anchor6;
+    @FXML
+	Button boton_combate1;
 
 
 
 	@FXML
 	public void initialize() {
+		Sexo1.setImage(p1.sexo);
+		Sexo2.setImage(p2.sexo);
+		Sexo3.setImage(p3.sexo);
+		Sexo4.setImage(p4.sexo);
+		Sexo5.setImage(p5.sexo);
+		Sexo6.setImage(p6.sexo);
 		l1Nombre1.setText(p1.nombre);
 		l2Nivel1.setText(""+p1.nivel);
 		l3Vida1.setText(""+p1.vida);
@@ -127,6 +148,7 @@ public class Ventana1Controller {
 		progress4.setProgress(p4.vida_actual/p4.vida);
 		progress5.setProgress(p5.vida_actual/p5.vida);
 		progress6.setProgress(p6.vida_actual/p6.vida);
+		boton_combate1.setDisable(true);
 
 
 	}
@@ -134,106 +156,116 @@ public class Ventana1Controller {
 	@FXML
 	private void clickear1() {
 		System.out.println("anchor pulsado");
-		Anchor1.setStyle("-fx-background-color:#1D6B7B;");
-		Anchor5.setStyle("-fx-background-color: #34B8D4;");
-		Anchor2.setStyle("-fx-background-color: #34B8D4;");
-		Anchor3.setStyle("-fx-background-color: #34B8D4;");
-		Anchor4.setStyle("-fx-background-color: #34B8D4;");
-		Anchor6.setStyle("-fx-background-color: #34B8D4;");
+		desclick();
+		Anchor1.setStyle("-fx-background-color:#675073");
 		img1.setStyle("-fx-opacity:1");
 		img6.setStyle("-fx-opacity:0.5");
 		img2.setStyle("-fx-opacity:0.5");
 		img3.setStyle("-fx-opacity:0.5");
 		img4.setStyle("-fx-opacity:0.5");
 		img5.setStyle("-fx-opacity:0.5");
+		boton_combate1.setDisable(false);
+		boton_combate1.setStyle("-fx-opacity:1");
 		cont=1;
 	}
 	@FXML
 	private void clickear2() {
 		System.out.println("anchor pulsado");
-		Anchor2.setStyle("-fx-background-color:#1D6B7B;");
-		Anchor1.setStyle("-fx-background-color: #34B8D4;");
-		Anchor5.setStyle("-fx-background-color: #34B8D4;");
-		Anchor3.setStyle("-fx-background-color: #34B8D4;");
-		Anchor4.setStyle("-fx-background-color: #34B8D4;");
-		Anchor6.setStyle("-fx-background-color: #34B8D4;");
+		desclick();
+		Anchor2.setStyle("-fx-background-color:#675073;");
 		img2.setStyle("-fx-opacity:1");
 		img1.setStyle("-fx-opacity:0.5");
 		img6.setStyle("-fx-opacity:0.5");
 		img3.setStyle("-fx-opacity:0.5");
 		img4.setStyle("-fx-opacity:0.5");
 		img5.setStyle("-fx-opacity:0.5");
+		boton_combate1.setDisable(false);
+		boton_combate1.setStyle("-fx-opacity:1");
 		cont=1;
 	}
 	@FXML
 	private void clickear3() {
 		System.out.println("anchor pulsado");
-		Anchor3.setStyle("-fx-background-color:#1D6B7B;");
-		Anchor1.setStyle("-fx-background-color: #34B8D4;");
-		Anchor2.setStyle("-fx-background-color: #34B8D4;");
-		Anchor5.setStyle("-fx-background-color: #34B8D4;");
-		Anchor4.setStyle("-fx-background-color: #34B8D4;");
-		Anchor6.setStyle("-fx-background-color: #34B8D4;");
+		desclick();
+		Anchor3.setStyle("-fx-background-color:#675073;");
 		img3.setStyle("-fx-opacity:1");
 		img1.setStyle("-fx-opacity:0.5");
 		img2.setStyle("-fx-opacity:0.5");
 		img6.setStyle("-fx-opacity:0.5");
 		img4.setStyle("-fx-opacity:0.5");
 		img5.setStyle("-fx-opacity:0.5");
+		boton_combate1.setDisable(false);
+		boton_combate1.setStyle("-fx-opacity:1");
+
 		cont=1;
 	}
 	@FXML
 	private void clickear4() {
 		System.out.println("anchor pulsado");
-		Anchor4.setStyle("-fx-background-color:#1D6B7B;");
-		Anchor1.setStyle("-fx-background-color: #34B8D4;");
-		Anchor2.setStyle("-fx-background-color: #34B8D4;");
-		Anchor3.setStyle("-fx-background-color: #34B8D4;");
-		Anchor5.setStyle("-fx-background-color: #34B8D4;");
-		Anchor6.setStyle("-fx-background-color: #34B8D4;");
+		desclick();
+		Anchor4.setStyle("-fx-background-color:#675073;");
 		img4.setStyle("-fx-opacity:1");
 		img1.setStyle("-fx-opacity:0.5");
 		img2.setStyle("-fx-opacity:0.5");
 		img3.setStyle("-fx-opacity:0.5");
 		img6.setStyle("-fx-opacity:0.5");
 		img5.setStyle("-fx-opacity:0.5");
+		boton_combate1.setDisable(false);
+		boton_combate1.setStyle("-fx-opacity:1");
 		cont=1;
 	}
 	@FXML
 	private void clickear5() {
 		System.out.println("anchor pulsado");
-		Anchor5.setStyle("-fx-background-color:#1D6B7B;");
-		Anchor1.setStyle("-fx-background-color: #34B8D4;");
-		Anchor2.setStyle("-fx-background-color: #34B8D4;");
-		Anchor3.setStyle("-fx-background-color: #34B8D4;");
-		Anchor4.setStyle("-fx-background-color: #34B8D4;");
-		Anchor6.setStyle("-fx-background-color: #34B8D4;");
+		desclick();
+		Anchor5.setStyle("-fx-background-color:#675073;");
 		img5.setStyle("-fx-opacity:1");
 		img1.setStyle("-fx-opacity:0.5");
 		img2.setStyle("-fx-opacity:0.5");
 		img3.setStyle("-fx-opacity:0.5");
 		img4.setStyle("-fx-opacity:0.5");
 		img6.setStyle("-fx-opacity:0.5");
-
+		boton_combate1.setDisable(false);
+		boton_combate1.setStyle("-fx-opacity:1");
 		cont=1;
 	}
 	@FXML
 	private void clickear6() {
 		System.out.println("anchor pulsado");
-		Anchor6.setStyle("-fx-background-color: #1D6B7B;");
-		Anchor1.setStyle("-fx-background-color: #34B8D4;");
-		Anchor2.setStyle("-fx-background-color: #34B8D4;");
-		Anchor3.setStyle("-fx-background-color: #34B8D4;");
-		Anchor4.setStyle("-fx-background-color: #34B8D4;");
-		Anchor5.setStyle("-fx-background-color: #34B8D4;");
+		desclick();
+		Anchor6.setStyle("-fx-background-color: #675073;");
 		img6.setStyle("-fx-opacity:1");
 		img1.setStyle("-fx-opacity:0.5");
 		img2.setStyle("-fx-opacity:0.5");
 		img3.setStyle("-fx-opacity:0.5");
 		img4.setStyle("-fx-opacity:0.5");
 		img5.setStyle("-fx-opacity:0.5");
+		boton_combate1.setDisable(false);
+		boton_combate1.setStyle("-fx-opacity:1");
 		cont=1;
 	}
+	@FXML
+	private void pelea(){
+		System.out.println("Boton pulsado pasamos al siguiente escenario");
+		Anchor6.setStyle("-fx-background-color: #7D6A87;");
+		Anchor1.setStyle("-fx-background-color: #7D6A87;");
+		Anchor2.setStyle("-fx-background-color: #7D6A87;");
+		Anchor3.setStyle("-fx-background-color: #7D6A87;");
+		Anchor4.setStyle("-fx-background-color: #7D6A87;;");
+		Anchor5.setStyle("-fx-background-color: #7D6A87;;");
+
+	}
+	private void desclick(){
+		Anchor6.setStyle("-fx-background-color: #7D6A87;");
+		Anchor1.setStyle("-fx-background-color: #7D6A87;");
+		Anchor2.setStyle("-fx-background-color: #7D6A87;");
+		Anchor3.setStyle("-fx-background-color: #7D6A87;");
+		Anchor4.setStyle("-fx-background-color: #7D6A87;;");
+		Anchor5.setStyle("-fx-background-color: #7D6A87;;");
+
+	}
+
+
 
 }
 class Pokemon{
@@ -241,13 +273,16 @@ class Pokemon{
 	Float vida;
 	Float vida_actual;
 	String nivel;
+	Image sexo;
 	Image foto;
-	Pokemon(String nombre,Float vida,Float vida_actual,String nivel,Image foto){
+
+	Pokemon(String nombre,Float vida,Float vida_actual,String nivel,Image foto,Image sexo){
 		this.nombre=nombre;
 		this.vida=vida;
 		this.vida_actual=vida_actual;
 		this.nivel=nivel;
 		this.foto=foto;
+		this.sexo=sexo;
 	}
 
 
